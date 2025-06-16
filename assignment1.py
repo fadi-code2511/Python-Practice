108652155e29e6f6bb0a16b9a99fb0a22c89b797
Fadi nasr <nasr.fadi2511@gmail.com>
2025-06-16 08:43:59 +0300

    adding the requirements as a comments

---
+++ assignment1.py
@@ -0,0 +1,21 @@
+# Requirements:
+# Start with a balance (e.g. $1000 stored in a variable).
+# Show a menu in a loop:
+# Welcome to the ATM
+# 1. Check Balance
+# 2. Deposit Money
+# 3. Withdraw Money
+# 4. Exit
+
+
+# Based on user input:
+# 1. Show the current balance.
+# 2. Ask how much to deposit, and add it to the balance.
+# 3. Ask how much to withdraw:
+
+# If enough balance → subtract and show new balance.
+# Else → show an error message.
+
+# 4. Exit the loop and end the program.
+
+# Validate input where needed (e.g., no negative deposits or withdrawals).
