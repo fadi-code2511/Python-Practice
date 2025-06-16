2ae2cd8915de47d470737bb0054064766bc9e6e8
Fadi nasr <nasr.fadi2511@gmail.com>
2025-06-16 11:51:04 +0300

    adding the loging password

--- assignment1.py
+++ assignment1.py
@@ -28,37 +28,40 @@
 balance=1000
 count=0
 withdrawAttempsCounter=0
-while True:
-    print( "\nWelcome to the ATM  \n1. Check Balance \n2. Deposit Money \n3. Withdraw Money \n4. Exit") # showing the menu
-    option=int(input("Enter the number of the option: "))
+password=int(input("Enter the password to log in: "))
+if password==120120:
+    while True:
+        print( "\nWelcome to the ATM  \n1. Check Balance \n2. Deposit Money \n3. Withdraw Money \n4. Exit") # showing the menu
+        option=int(input("Enter the number of the option: "))
 
-    if option==1:
-        print("your current balance is: ",balance,)   #printing the balance
-    elif option==2:
-        DepositAmount=int(input("Enter the amount you want to deposit? "))
-        DepositAmount=abs(DepositAmount) # to make sure the number always postive
-        balance=balance+DepositAmount #adding the amount to the balance
-        
-    elif option==3:
-        WithDrawAmount=int(input("Enter the amount you want to withdraw? "))
-        WithDrawAmount=abs(WithDrawAmount) # to make sure the number always postive
-        if WithDrawAmount<balance:
-         balance=balance-WithDrawAmount # deducting the amount from the balance
+        if option==1:
+            print("your current balance is: ",balance,)   #printing the balance
+        elif option==2:
+            DepositAmount=int(input("Enter the amount you want to deposit? "))
+            DepositAmount=abs(DepositAmount) # to make sure the number always postive
+            balance=balance+DepositAmount #adding the amount to the balance
+            
+        elif option==3:
+            WithDrawAmount=int(input("Enter the amount you want to withdraw? "))
+            WithDrawAmount=abs(WithDrawAmount) # to make sure the number always postive
+            if WithDrawAmount<balance:
+                balance=balance-WithDrawAmount # deducting the amount from the balance
+            else:
+                print("no sufficient balance to withdraw,your balance is ",balance)
+                withdrawAttempsCounter=withdrawAttempsCounter+1 #counter for the withdraw attemps
+                if withdrawAttempsCounter>3:
+                    print("Warning: you have tried more than 3 attemps")
+                    break   #closing the program
+    
+        elif option==4:
+            break
         else:
-            print("no sufficient balance to withdraw,your balance is ",balance)
-            withdrawAttempsCounter=withdrawAttempsCounter+1 #counter for the withdraw attemps
-            if withdrawAttempsCounter>3:
-                print("Warning, you have tried more than 3 attemps")
-                break
+            print("you entered a wrong number\n")
 
-    elif option==4:
-        break
-    else:
-        print("you entered a wrong number\n")
-
-    count=count+1  #adding the counter
-    print("\nyou have done ",count," transactions ")
-
+        count=count+1  #adding the counter
+        print("\nyou have done ",count," transactions ")
+else:
+    print("you have entered a wrong password")
 
 
 #          Bonus Challenges (Optional):
